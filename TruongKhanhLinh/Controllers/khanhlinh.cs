02af0a0019c24ad86a8699a130e4dd393a395d70
tvqua <tvqua@LAPTOP-HLCS8RCT>
2023-09-15 19:01:34 +0700

    Add project files.

---
+++ TruongKhanhLinh/Controllers/khanhlinh.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TruongKhanhLinh.Controllers
+{
+    public class khanhlinh : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = " ";
+            return View();
+        }
+        public IActionResult Index(int hoten, int ngaysinh, int email, int sodienthoai)
+        {
+            ViewData["hoten"] = hoten;
+            ViewData["ngaysinh"] = ngaysinh;
+            ViewData["email"] = email;
+            ViewData["sodienthoai"] = sodienthoai;
+            return View();
+        }
+    }
+}
