5b21d89e30d09ca1ffbd17d92c7a1ee689c6287d
khnhlynh <linhtruong512008@gmail.com>
2023-09-15 19:28:55 +0700

    edit

---
+++ TruongKhanhLinh/Controllers/ModelDanhSach.cs
@@ -0,0 +1,16 @@
+﻿namespace TruongKhanhLinh.Controllers
+{
+    public class ModelDanhSach
+    {
+        private string tensanpham;
+        private int soluong;
+        private int dongia;
+
+        public ModelDanhSach(string tensanpham, int soluong, int dongia, int giamgia)
+        {
+            this.tensanpham = tensanpham;
+            this.soluong = soluong;
+            this.dongia = dongia;
+        }
+    }
+}
---
+++ TruongKhanhLinh/Controllers/linh.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TruongKhanhLinh.Controllers
+{
+    public class linh : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        private static List<ModelDanhSach> products = new();
+
+        public static List<ModelDanhSach> Products { get => products; set => products = value; }
+
+        public IActionResult Index(bool? check = false)
+        {
+            if ((bool)check)
+            {
+                products.Clear();
+            }
+            else
+            {
+                return View(products.ToList());
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string tensanpham, int soluong, int dongia, int giamgia)
+        {
+            var homeProduct = new ModelDanhSach(tensanpham,soluong,dongia, giamgia);
+            products.Add(homeProduct);
+            return View(products);
+        }
+
+        public IActionResult RemoveProduct(int position)
+        {
+            products.RemoveAt(position - 1);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
